106472e896d3ef9a648cfaf5c0fd86c80f71f81e
Kandha Sankarapandian <ks.kandha@gmail.com>
2020-05-20 21:53:31 -0700

    Embed current video url in iframe

--- xfxpal-talent/src/Videos.js
+++ xfxpal-talent/src/Videos.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
-
+const style = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: 'calc(100vh - 60px)',
+    },
+    iframe: {
+        border: 0,
+        flexGrow: 1,
+    }
+}
 export default () => {
     return (
-        <>
+        <div style={style.container}>
             <Header as="h1" textAlign="center">Videos showcasing the work done at FXPAL</Header>
-            <iframe title="FXPAL playlist" style={{ border: 0, width: '100%', height: '500px' }} src="http://www.youtube.com/embed/?listType=user_uploads&list=FXPAL" ></iframe>
-        </>
+            <iframe title='Videos' src='https://xfxpal.com/videos' style={style.iframe} allowFullScreen></iframe>
+        </div>
     )
 }
